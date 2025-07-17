01ab588c0a4f89e802c5717d399130cde7200be3
Sergio Sainz <100535668+sergiolplp@users.noreply.github.com>
2025-07-16 22:51:07 -0300

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ const CACHE_NAME = 'torneo-cache-v1';
 const urlsToCache = [
   './',
   './index.html',
-  './ligas.html',
+  './liga.html',
   './torneoparejas.html',
   './historico.html',
   './icono_torneo.ico',
